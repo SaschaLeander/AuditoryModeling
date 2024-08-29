00916107fabd99e822e920d258fe0c296e029d3a
gegy9991 <sup.official.99@gmail.com>
2024-08-29 13:31:39 +0100

    comparison plot/run model update

---
+++ plot_neurogram_comparison.m
@@ -0,0 +1,332 @@
+%% Function plots auditory response =======================
+
+% written by Sam Jones and Sascha Muhlinghaus(30/07/2024)
+% contact: saschamuhlinghaus@gmail.com
+
+function plot_neurogram_comparison(stimulus, r_mean, ihc, ic_sout_BE, cn_sout_contra, Fs, cf, stimulus2, r_mean2, ihc2, ic_sout_BE2, cn_sout_contra2, varArray, nsimArray, ssimArray)
+
+    % re-size matrices, accounting for zero-padding within the model
+    [sizeN, sizeM] = size(r_mean);
+    ihc = ihc(1:sizeN, 1:sizeM);
+    ic_sout_BE = ic_sout_BE(1:sizeN, 1:sizeM);
+    cn_sout_contra = cn_sout_contra(1:sizeN, 1:sizeM);
+
+    [sizeN2, sizeM2] = size(r_mean2);
+    ihc2 = ihc2(1:sizeN2, 1:sizeM2);
+    ic_sout_BE2 = ic_sout_BE2(1:sizeN2, 1:sizeM2);
+    cn_sout_contra2 = cn_sout_contra2(1:sizeN2, 1:sizeM2);
+    
+    % define common time vector
+    t = (1:length(r_mean))*1/Fs;
+    t2 = (1:length(r_mean2))*1/Fs;
+
+    % plot figures
+    figure;
+    
+    % raw stimulus
+    subplot(5, 5, 1)
+    t_stimulus = 1/Fs:1/Fs:length(r_mean)/Fs;
+    minLength = min(length(t_stimulus), length(stimulus));
+    t_stimulus = t_stimulus(1:minLength);
+    stimulus = stimulus(1:minLength);
+    plot(t_stimulus, stimulus);
+    title('Stimulus waveform (reference)');
+    xlabel('Time (s)');
+    ylabel('Amplitude (Pa)');
+    grid on;
+
+    % raw stimulus
+    subplot(5, 5, 2)
+    t_stimulus = 1/Fs:1/Fs:length(r_mean2)/Fs;
+    minLength = min(length(t_stimulus), length(stimulus2));
+    t_stimulus = t_stimulus(1:minLength);
+    stimulus2 = stimulus2(1:minLength);
+    plot(t_stimulus, stimulus2);
+    title('Stimulus waveform (altered)');
+    xlabel('Time (s)');
+    ylabel('Amplitude (Pa)');
+    grid on;
+
+    % inner hair cell response
+    subplot(5, 5, 3)
+    plot(t, ihc);
+    xlim([0 (length(stimulus)/Fs)*1.2]) %  scale x-axis by 1.2 * stimulus length
+    title('Inner hair cell response (reference)')
+    xlabel('Time (s)')
+    ylabel('Volts')
+    grid on
+
+    % inner hair cell response 2
+    subplot(5, 5, 4)
+    plot(t2, ihc2);
+    xlim([0 (length(stimulus)/Fs)*1.2]) %  scale x-axis by 1.2 * stimulus length
+    title('Inner hair cell response (altered)')
+    xlabel('Time (s)')
+    ylabel('Volts')
+    grid on
+
+    % auditory nerve mean rate by CF
+    r_clipped = r_mean(1:length(stimulus),:); 
+    r_mean_clipped = mean(r_clipped, 1);
+
+    r_clipped2 = r_mean2(1:length(stimulus2),:); 
+    r_mean_clipped2 = mean(r_clipped2, 1);
+
+    subplot(5, 5, 5);
+    semilogx(cf, r_mean_clipped, 'b-', 'LineWidth', 2);
+    hold on; % Hold on to add the second line to the same plot
+    semilogx(cf, r_mean_clipped2, 'r-', 'LineWidth', 2);
+    hold off;
+    xlabel('Frequency (Hz)')
+    ylabel('Mean rate (sp/s)')
+    title('Auditory nerve response (mean rate)')
+    xlim([0 8000]); 
+    legend('reference', 'altered');
+    set(gca, 'XTick', [20 50 100 200 500 1000 2000 5000 8000]);
+    set(gca, 'XTickLabel', {'20', '50', '100', '200', '500', '1k', '2k', '5k', '8k'});
+    grid on;
+
+    % frequency spectrum subplot
+    subplot(5, 5, 6)
+
+    % calculate FFT
+    n = length(stimulus);
+    frequencies = (0:n-1)*(Fs/n);
+    magnitude = abs(fft(stimulus))/n;
+    
+    % convert to dB SPL (assuming the reference pressure p0 = 20 µPa)
+    referencePressure = 20e-6;
+    magnitude_db_spl = 20 * log10(magnitude / referencePressure);
+    
+    % plot the magnitude in dB SPL by frequency
+    semilogx(frequencies(1:floor(n/2)), magnitude_db_spl(1:floor(n/2)));
+    xlim([125 8000]); 
+    xlabel('Frequency (Hz)')
+    ylabel('Magnitude (dB SPL)')
+    title('Stimulus frequency spectrum (reference)')
+    set(gca, 'XTick', [20 50 100 200 500 1000 2000 5000 8000]);
+    set(gca, 'XTickLabel', {'20', '50', '100', '200', '500', '1k', '2k', '5k', '8k'});
+    grid on;
+
+    % frequency spectrum subplot 2
+    subplot(5, 5, 7)
+
+    % calculate FFT
+    n = length(stimulus2);
+    frequencies = (0:n-1)*(Fs/n);
+    magnitude = abs(fft(stimulus2))/n;
+    
+    % convert to dB SPL (assuming the reference pressure p0 = 20 µPa)
+    referencePressure = 20e-6;
+    magnitude_db_spl = 20 * log10(magnitude / referencePressure);
+    
+    % plot the magnitude in dB SPL by frequency
+    semilogx(frequencies(1:floor(n/2)), magnitude_db_spl(1:floor(n/2)));
+    xlim([125 8000]); 
+    xlabel('Frequency (Hz)')
+    ylabel('Magnitude (dB SPL)')
+    title('Stimulus frequency spectrum (altered)')
+    set(gca, 'XTick', [20 50 100 200 500 1000 2000 5000 8000]);
+    set(gca, 'XTickLabel', {'20', '50', '100', '200', '500', '1k', '2k', '5k', '8k'});
+    grid on;
+
+    % auditory nerve response  1
+    subplot(5, 5, 8)
+    plot(t, r_mean)
+    title('Auditory nerve response')
+    xlim([0 length(stimulus)/Fs])
+    xlabel('Time (s)')
+    ylabel('Instantaneous rate (sp/s) (reference)')
+    grid on;
+
+    % auditory nerve response  2
+    subplot(5, 5, 9)
+    plot(t, r_mean2)
+    title('Auditory nerve response')
+    xlim([0 length(stimulus)/Fs])
+    xlabel('Time (s)')
+    ylabel('Instantaneous rate (sp/s) (altered)')
+    grid on;
+    
+    % cochlear nucleus response mean rate by CF
+    cn_clipped = cn_sout_contra(1:length(stimulus),:);
+    cn_mean_clipped = mean(cn_clipped, 1);
+
+    cn_clipped2 = cn_sout_contra2(1:length(stimulus2),:);
+    cn_mean_clipped2 = mean(cn_clipped2, 1);
+    subplot(5, 5, 10);
+    semilogx(cf, cn_mean_clipped, 'b-', 'LineWidth', 2); 
+    hold on;
+    semilogx(cf, cn_mean_clipped2, 'r-', 'LineWidth', 2); 
+    hold off;
+    xlabel('Frequency (Hz)')
+    ylabel('Mean rate (sp/s)')
+    title('Cochlear nucleus response (mean rate)')
+    legend('reference', 'altered');
+    xlim([0 8000]); 
+    set(gca, 'XTick', [20 50 100 200 500 1000 2000 5000 8000]);
+    set(gca, 'XTickLabel', {'20', '50', '100', '200', '500', '1k', '2k', '5k', '8k'});
+    grid on;
+
+    % auditory nerve response CF 1
+    subplot(5, 5, 11)
+    imagesc(t, log10(cf/1e3), r_mean');
+    axis xy;
+    yticks = [0.125 0.5 2 8 16];
+    set(gca, 'ytick', log10(yticks));
+    set(gca, 'yticklabel', yticks);
+    ylabel('CF (kHz)')
+    xlabel('Time (s)')
+    xlim([0 length(stimulus)/Fs])
+    hcb = colorbar;
+    set(get(hcb, 'ylabel'), 'string', 'Spikes / second');
+    title('Auditory nerve response (reference)')
+    grid on
+
+    % auditory nerve response CF 2
+    subplot(5, 5, 12)
+    imagesc(t2, log10(cf/1e3), r_mean2');
+    axis xy;
+    yticks = [0.125 0.5 2 8 16];
+    set(gca, 'ytick', log10(yticks));
+    set(gca, 'yticklabel', yticks);
+    ylabel('CF (kHz)')
+    xlabel('Time (s)')
+    xlim([0 length(stimulus)/Fs])
+    hcb = colorbar;
+    set(get(hcb, 'ylabel'), 'string', 'Spikes / second');
+    title('Auditory nerve response (altered)')
+    grid on;
+
+    % cochlear nucleus response 1
+    subplot(5, 5, 13)
+    plot(t, cn_sout_contra)
+    title('Cochlear nucleus response')
+    xlim([0 length(stimulus)/Fs])
+    xlabel('Time (s)')
+    ylabel('Instantaneous rate (sp/s) (reference)')
+    grid on;
+
+    % cochlear nucleus response 2
+    subplot(5, 5, 14)
+    plot(t, cn_sout_contra2)
+    title('Cochlear nucleus response')
+    xlim([0 length(stimulus)/Fs])
+    xlabel('Time (s)')
+    ylabel('Instantaneous rate (sp/s) (altered)')
+    grid on;
+
+    % inferior colliculus response mean rate by CF
+    ic_clipped = ic_sout_BE(1:length(stimulus),:);
+    ic_mean_clipped = mean(ic_clipped, 1); 
+
+    ic_clipped2 = ic_sout_BE2(1:length(stimulus2),:);
+    ic_mean_clipped2 = mean(ic_clipped2, 1);
+    subplot(5, 5, 15);
+    semilogx(cf, ic_mean_clipped, 'b-', 'LineWidth', 2); 
+    hold on;
+    semilogx(cf, ic_mean_clipped2, 'r-', 'LineWidth', 2); 
+    hold off;
+    xlabel('Frequency (Hz)');
+    ylabel('Mean rate (sp/s)');
+    legend('reference', 'altered');
+    title('Inferior colliculus response (mean rate)');
+    xlim([0 8000]); 
+    set(gca, 'XTick', [20 50 100 200 500 1000 2000 5000 8000]);
+    set(gca, 'XTickLabel', {'20', '50', '100', '200', '500', '1k', '2k', '5k', '8k'});
+    grid on;
+
+    % cochlear nucleus response CF 1
+    subplot(5, 5, 16)
+    imagesc(t, log10(cf/1e3), cn_sout_contra');
+    axis xy;
+    yticks = [0.125 0.5 2 8 16];
+    set(gca, 'ytick', log10(yticks));
+    set(gca, 'yticklabel', yticks);
+    ylabel('CF (kHz)')
+    xlabel('Time (s)')
+    xlim([0 length(stimulus)/Fs])
+    hcb = colorbar;
+    set(get(hcb, 'ylabel'), 'string', 'Spikes / second');
+    title('Cochlear nucleus response (reference)')
+    grid on;
+
+    % cochlear nucleus response CF 2
+    subplot(5, 5, 17)
+    imagesc(t2, log10(cf/1e3), cn_sout_contra2');
+    axis xy;
+    yticks = [0.125 0.5 2 8 16];
+    set(gca, 'ytick', log10(yticks));
+    set(gca, 'yticklabel', yticks);
+    ylabel('CF (kHz)')
+    xlabel('Time (s)')
+    xlim([0 length(stimulus2)/Fs])
+    hcb = colorbar;
+    set(get(hcb, 'ylabel'), 'string', 'Spikes / second');
+    title('Cochlear nucleus response (altered)')
+    grid on;
+
+    % inferior colliculus response 1
+    subplot(5, 5, 18)
+    plot(t, ic_sout_BE)
+    xlim([0 length(stimulus)/Fs])
+    xlabel('Time (s)')
+    ylabel('Instantaneous rate (sp/s)')
+    title('Inferior colliculus response (reference)')
+    grid on;
+
+    % inferior colliculus response 2
+    subplot(5, 5, 19)
+    plot(t2, ic_sout_BE2)
+    xlim([0 length(stimulus2)/Fs])
+    xlabel('Time (s)')
+    ylabel('Instantaneous rate (sp/s)')
+    title('Inferior colliculus response (altered)')
+    grid on;
+    
+    subplot(5, 5, 20);
+    plot(varArray, nsimArray, 'r-', 'LineWidth', 2); % Plot the first line in red
+    hold on; % Hold on to add the second line to the same plot
+    plot(varArray, ssimArray, 'b-', 'LineWidth', 2); % Plot the first line in green
+    hold off;  
+    % Add labels and title
+    xlabel('Signal Frequency/Amplitude [Hz/dB SPL]');
+    ylabel('Similarity Metric [0 - 1]');
+    legend('reference', 'altered');
+    title('Neurogram Similarity');
+    % Add legend
+    legend('NSIM','SSIM'); 
+    % Show grid
+    grid on;
+
+    % inferior colliculus response CF 1
+    subplot(5, 5, 21)
+    imagesc(t, log10(cf/1e3), ic_sout_BE');
+    axis xy;
+    yticks = [0.125 0.5 2 8 16];
+    set(gca, 'ytick', log10(yticks));
+    set(gca, 'yticklabel', yticks);
+    ylabel('CF (kHz)')
+    xlabel('Time (s)')
+    xlim([0 length(stimulus)/Fs])
+    hcb = colorbar;
+    set(get(hcb, 'ylabel'), 'string', 'Spikes / second');
+    title('Inferior colliculus response (reference)')
+    grid on;
+
+    % inferior colliculus response CF 2
+    subplot(5, 5, 22)
+    imagesc(t2, log10(cf/1e3), ic_sout_BE2');
+    axis xy;
+    yticks = [0.125 0.5 2 8 16];
+    set(gca, 'ytick', log10(yticks));
+    set(gca, 'yticklabel', yticks);
+    ylabel('CF (kHz)')
+    xlabel('Time (s)')
+    xlim([0 length(stimulus2)/Fs])
+    hcb = colorbar;
+    set(get(hcb, 'ylabel'), 'string', 'Spikes / second');
+    title('Inferior colliculus response (altered)')
+    grid on;
+
+end
---
+++ run_modelv2.m
@@ -0,0 +1,217 @@
+%% Script runs stimulus, generation, zilany2014, carney2015 and stores neurogram similarity ==================================================================
+
+% code written by Sam Jones and Sascha Muhlinghaus
+
+%% set path, start modified amt ===========================================
+
+addpath 'amtoolbox-full-1.5.0/amtoolbox-full-1.5.0'
+
+amt_start;
+
+amt_mex;
+
+%% sanity check - ensure modified models work as expected
+
+% demo_zilany2014; % run and compare to link below 
+% https://amtoolbox.org/amt-1.5.0/doc/demos/demo_zilany2014.php 
+
+% demo_carney2015; % run and compare to link below
+% https://amtoolbox.org/amt-1.5.0/doc/demos/demo_carney2015.php 
+
+%% system configuration ===================================================
+function [nsimArray, ssimArray, varArray] = run_analysis(numCF_input, condition_input, dependent_var_input, limit_input, stepper_input, status_input, varargin)
+
+    Fs = 100e3; % "model sampling frequency" [samples/sec]
+    
+    %% stimulus params =====================================================
+    condition = condition_input;  % condition_flag: 'forward masking', 'backward masking', 'simultaneous', 'notch', 'frequency discrimination'
+    signal_length_ms = 20; % signal_length_ms: length of the stimulus in (ms)
+    switch condition
+        case 'frequency discrimination'
+            dB_signal = 70; % dB_signal: desired decibel level of the tone
+            signal_freq = dependent_var_input; % tone_freq: frequency of the tone in Hz
+    
+        otherwise
+            dB_signal = dependent_var_input; % dB_signal: desired decibel level of the tone
+            signal_freq = 1000; % tone_freq: frequency of the tone in Hz
+    end
+    
+    db_noise = 30; % varargin1: inputs depending on the chosen condition
+    gap = 50; % varargin2: inputs depending on the chosen condition
+    vcv_string = 'a b a';
+    debug_flag = false;
+    
+    %% generate a neurogram for a specific hearing loss =======================
+    
+    ag_dbloss = [0 0 0 0 0 0 0]; % modify loss here (+ db, linked to ag_fs)
+    ag_fs = [125 250 500 1e3 2e3 4e3 8e3]; % audiometric frequencies
+    
+    %% define auditory pathway params (AN, CN, IC) ========================
+    
+    species = 2; % 1 = cat; 2 = human
+    numCF = numCF_input; % number of inner hair cells
+    CF_range = [125 14e3]; % frequency range
+    cf = audspace(CF_range(1),CF_range(2),numCF); % generate cfs
+      
+    fiberType = 1; % AN fiber type, 1 = L, 2 = M, 3 = H spontaneous rate, 4 = ratio defined below
+    fiber_num = 20; % number of fibres
+    numH = 10; % high spontaneous rate fibres
+    numM = 5; % medium spontaneous rate fibres
+    numL = 5; % low spontaneous rate fibres
+    BMF = 100; % default CN/IN best modulation frequency
+    
+    %% run models and save data =============================================================
+    limit = limit_input;
+    stepper = stepper_input;
+    
+    % Which metric is computed
+    flag = 'both';
+    
+    % Number of iterations for plotting neurogram
+    plot_every = 20;
+    
+    % Dependent variable
+    dependent_var = dependent_var_input; % Either dB_signal or signal_freq
+    
+    % Number of generated samples
+    samples = abs(abs(dependent_var - limit) / stepper);
+    
+    fprintf('Number of samples: %d\n', samples);
+    
+    % Initialize arrays for similarity function
+    nsimArray = [];
+    ssimArray = [];
+    
+    varArray = [];
+    
+    % Choose degraded or reference condition based on input
+    if status_input == 2
+        fprintf('Status: %s\n', 'degraded');
+        bm_deg = varargin{1}; % scale basilar membrane tuning, 1 = normal, > 1 = broader
+        cohc_deg = varargin{2}; % OHC function scaling factor: 1 denotes normal function
+        cihc_deg = varargin{3}; % IHC function scaling factor: 1 denotes normal function
+        bm_current = bm_deg;
+        cohc_current = cohc_deg;
+        cihc_current = cihc_deg;
+    else
+        fprintf('Status: %s\n', 'control');
+        bm_current = 1;
+        cohc_current = 1;
+        cihc_current = 1;
+    end
+    
+    % Generate stimulus 1 (reference)
+    switch condition
+        case 'vcv'
+            [stimulus1, ~, ~] = generate_stimulus(condition, ...
+                signal_freq, signal_length_ms, dependent_var, debug_flag, vcv_string);
+        case 'frequency discrimination'
+            [stimulus1, ~, ~] = generate_stimulus(condition, ...
+                dependent_var, signal_length_ms, dB_signal, debug_flag, db_noise, gap);
+        otherwise
+            [stimulus1, ~, ~] = generate_stimulus(condition, ...
+                signal_freq, signal_length_ms, dependent_var, debug_flag, db_noise, gap);
+    end
+    
+    % Model auditory response
+    [r_mean1, ihc1, ic_sout_BE1, cn_sout_contra1] = simulate_auditory_response(stimulus1, ...
+        Fs, cf, bm_current, fiberType, numH, numM, numL, fiber_num, cohc_current, cihc_current, BMF);
+    
+    % Plot reference neurogram
+    plot_neurograms(stimulus1, r_mean1, ihc1, ic_sout_BE1, cn_sout_contra1, Fs, cf);
+    
+    for i = 1:samples
+        
+        % Update dependent variable 
+        dependent_var = dependent_var + stepper;
+        
+        fprintf('dependent variable (dB/Hz): %d\n', dependent_var);
+        varArray = [varArray; dependent_var];
+           
+        % Generate stimulus 2 (altered)
+        switch condition
+            case 'vcv'
+                [stimulus2, ~, ~] = generate_stimulus(condition, ...
+                    signal_freq, signal_length_ms, dependent_var, debug_flag, vcv_string);
+            case 'frequency discrimination'
+                [stimulus2, ~, ~] = generate_stimulus(condition, ...
+                    dependent_var, signal_length_ms, dB_signal, debug_flag, db_noise, gap);
+            otherwise
+                [stimulus2, ~, ~] = generate_stimulus(condition, ...
+                    signal_freq, signal_length_ms, dependent_var, debug_flag, db_noise, gap);
+        end
+        
+        % Model auditory response for altered stimulus
+        [r_mean2, ihc2, ic_sout_BE2, cn_sout_contra2] = simulate_auditory_response(stimulus2, ...
+            Fs, cf, bm_current, fiberType, numH, numM, numL, fiber_num, cohc_current, cihc_current, BMF);
+        
+        if mod(i, plot_every) == 0
+            % Plot neurogram 2 (altered)
+            %plot_neurograms(stimulus2, r_mean2, ihc2, ic_sout_BE2, cn_sout_contra2, Fs, cf);
+        end
+        
+        % Compute similarity scores
+        [nsim_score, ssim_score] = compute_similarity(ic_sout_BE1, ic_sout_BE2, flag);
+        nsimArray = [nsimArray; nsim_score];
+        ssimArray = [ssimArray; ssim_score];
+        
+        % Save parameters and data to csv file
+        switch condition
+            case 'frequency discrimination'
+                manage_data(i, dB_signal, dependent_var, cihc_current, cohc_current, ...
+                    bm_current, condition, dependent_var_input, nsim_score, ssim_score);
+            otherwise
+                manage_data(i, dependent_var, signal_freq, cihc_current, cohc_current, ...
+                    bm_current, condition, dependent_var_input, nsim_score, ssim_score);
+        end
+    end  
+    % Plot comparison neurogram plot 
+    plot_neurogram_comparison(stimulus1, r_mean1, ihc1, ic_sout_BE1, cn_sout_contra1, ...
+        Fs, cf, stimulus2, r_mean2, ihc2, ic_sout_BE2, cn_sout_contra2, ...
+        varArray, nsimArray, ssimArray);
+end
+
+
+%% Call function ==========================================================
+% bm_params = [3,1,1,3];
+% cihc_params = [1,0.3,1,0.3];
+% cohc_params = [1,1,0.3,0.3];
+% 
+% 
+% titles = {
+%     ['Neurogram Similarity, numCF=', num_cf ,', BM = 3'], ...
+%     ['Neurogram Similarity, numCF=', num_cf ,', cihc = .3'], ...
+%     ['Neurogram Similarity, numCF=', num_cf ,', cohc = .3'], ...
+%     ['Neurogram Similarity, numCF=', num_cf ,', full impairment bm = 3, cihc = .3, cohc = .3']};
+% 
+% condition_params = {'frequency discrimination', 'vcv'};
+% 
+% limit_dependent_var = [1500, 30];
+% change_rate_dep_var = [10, -5];
+% dependent_variable = [1000, 80];
+
+function wrapper(bm, cihc, cohc, num_cf, condition, dependent_variable, limit_dependent_var, change_rate_dep_var)
+    
+    status = 1;
+    
+    [nsimArray_ref, ssimArray_ref, varArray_ref] = run_analysis(num_cf, ...
+        condition, dependent_variable, limit_dependent_var, change_rate_dep_var, status);
+    
+    status = 2;
+
+    [nsimArray_degraded, ssimArray_degraded, varArray_degraded] = run_analysis( num_cf, ...
+        condition, dependent_variable, limit_dependent_var, change_rate_dep_var, status, bm, cihc, cohc);
+end 
+
+bm = 3;
+cohc = 1;
+cihc = 1;
+num_cf = 100;
+condition = 'frequency discrimination';
+dependent_variable = 1000;
+limit_dependent_var = 1500;
+change_rate_dep_var = 10;
+title_input = 'Neurogram Similarity, numCF=5, full impairment bm = 3, cihc = .3, cohc = .3';
+
+wrapper(bm, cohc, cihc, num_cf, condition, dependent_variable, limit_dependent_var, change_rate_dep_var);
+
